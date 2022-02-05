dcd7c668371c1ddf8940c3398f0657fb78a98d6c
NFoithong <n.foithong1983@gmail.com>
2022-02-05 14:11:06 -0500

    create generate html

--- src/page-template.js
+++ src/page-template.js
@@ -1,81 +1,111 @@
 const fs = require('fs');
 
-const generateInfo = infoArr => {
-    
-    return `
-    <section class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-      <div class="flex-row justify-space-between">
-      ${infoArr
-        .map(({ name, id, emailAddress, officeNumber, githubUsername, school }) => {
-          return `
-          <div class="col-12 mb-2 bg-dark text-light p-3">
-            <h3 class="portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-              ID:
-              ${id}
-            </h5>
-            <h5 class="portfolio-languages">
-              Email: 
-              <a href="mailto:${emailAddress}" target="_blank">${emailAddress}</a>
-            </h5>
-            <h5 class="portfolio-languages">
-              Office number: 
-              ${officeNumber}
-            </h5>
-            <h5 class="portfolio-languages">
-              Github: 
-              ${githubUsername}
-            </h5>
-            <h5 class="portfolio-languages">
-              School: 
-              ${school}
-            </h5>
+const generateInfo = (team)=> {
+  console.log(team);
+
+    const html =[];
+
+    const generateManager = manager => {
+      console.log(manager);
+      
+      let managerHTML = `
+        <div class='card' style='width:18rem;'>
+          <div class='card-header'>
+            ${manager.name} <br>
+            <i class="fas fa-coffee"></i>Manager
           </div>
-        `;
-        })
-        .join('')}
-  `;
-};
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${manager.id} </li>
+            <li class="list-group-item">Email: <span id='email'><a href='mailto:${manager.emailAddress}'> ${manager.emailAddress}</a></span></li>
+            <li class="list-group-item">Office Number: ${manager.officeNumber} </li>
+          </ul>
+        </div>
+      `;
+      html.push(managerHTML);
+    }
 
-// module.exports = teamData => {
-//     console.log(teamData);
+    const generateEngineer = engineer => {
+      console.log(engineer);
+      
+      let engineerHTML = `
+        <div class='card' style='width:18rem;'>
+          <div class='card-header'>
+            ${engineer.name} <br>
+            <i class="fas fa-laptop-code"></i>Engineer
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${engineer.id} </li>
+            <li class="list-group-item">Email: <span id='email'><a href='mailto:${engineer.emailAddress}'> ${engineer.emailAddress}</a></span></li>
+            <li class="list-group-item">Github Username: <a target='_blank' href='https://github.${engineer.githubUsername}'>${engineer.githubUsername} </a></li>
+          </ul>
+        </div>
+      `;
+      html.push(engineerHTML);
+    }
 
-//     const { project...header } = teamData;
+    const generateIntern = intern => {
+      console.log(intern);
 
-//     return `
-//     <!DOCTYPE html>
-//     <html lang='en'>
-    
-//     <head>
-//         <meta charset="UTF-8">
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//         <title>NF Team Profile Generator</title>
-//         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-//         <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-//         <link rel="stylesheet" href="style.css">
-//         </head>
+      let internHTML = `
+        <div class='card' style='width:18rem;'>
+          <div class='card-header'>
+            ${intern.name} <br>
+            <i class="fas fa-user-graduate"></i>Intern
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.id} </li>
+            <li class="list-group-item">Email: <span id='email'><a href='mailto:${intern.emailAddress}'> ${intern.emailAddress}</a></span></li>
+            <li class="list-group-item">School: ${intern.school} </li>
+          </ul>
+        </div>
+      `;
+      html.push(internHTML);
+    }
 
-//     <body>
-//     <header>
-//     <div class="container flex-row justify-space-between align-center py-3">
-//       <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
-//     </div>
-//     </header>
-//     <main class="container my-5">
-//     ${generateInfo}
-//   </main>
-//   <footer class="container text-center py-3">
-//   </footer>
-//   </body>
-//   </html>
-//   `;
-//   };
+    //create a loop for all of the team
+    for (let i = 0; i<team.length; i++) {
+      if (team[i].getRole() === 'Manager') {
+        generateManager(team[i]);
+      }
+      if (team[i].getRole() === 'Engineer') {
+        generateEngineer(team[i]);
+      }
+      if (team[i].getRole() === 'Intern') {
+        generateIntern(team[i]);
+      } 
+    }
 
-  fs.writeFile('./index.html', generateInfo(infoArr), err => {
-    if (err) throw err;
-    console.log('generate complete! Check out index.html')
-});
+    //join the html
+    return html.join('');
+  }
 
-module.exports = generateInfo;
+module.exports = team => {
+
+  return `
+  <!DOCTYPE html>
+  <html lang='en'>
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>NF Team Profile Generator</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>  
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+      </head>
+
+  <body>
+  <header>
+    <h1> My Team </h1>
+  </header>
+  <main>
+  ${generateInfo(team)}
+</main>
+
+<script src="https://kit.fontawesome.com/493bea5fd9.js" crossorigin="anonymous"></script>
+</body>
+</html>
+`;
+}
